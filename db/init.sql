dad6544b61fe20d6823cc97807e9f157883951b2
khaleelElias <you@example.com>
2024-03-11 21:29:45 +0100

    docker added

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+-- Create a table to store user accounts in. 
+CREATE TABLE IF NOT EXSISTS accounts (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(25) NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    CONSTRAINT unsernameUnique UNIQUE (username)
+);
+
+--Create a table to store stores in.
+CREATE TABLE IF NOT EXISTS stores (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    
+);
